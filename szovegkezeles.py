f2a2f88ee8ca567cc8f83d7d8c2e31b789fc74b6
Gados Dominik <gados.dominik@ckik.loc>
2025-10-16 10:10:11 +0200

    hazi 10.16

--- szovegkezeles.py
+++ szovegkezeles.py
@@ -1,7 +1,15 @@
+import random
+
 # lebegőpopntos - float - tört
 a = 1.25
 b = float(input("adjon meg egy tizedes törtet: "))
 print(b*4)
 
+#Generáljon ki {1,10} közötti tört számot 2 tizedesjegyre
+# pl:1.10, 2.30
 
-# generáljon ki [1,9] közötti
+#c = random.randint(100,999)/100
+c = random.random() #[0,1[
+print(round(c,2))
+#hf befejezni
+
