d3cd16513a68378ea03da652165a75da5d2556b2
Gados Dominik <gados.dominik@ckik.loc>
2025-11-07 11:03:01 +0100

    kalapacs

--- ciklusok.py
+++ ciklusok.py
@@ -22,12 +22,49 @@ print( )
 
 for paros in range(0,19,2):
     print(paros,end=" ")
-
+print()
 
 # szöveg betűi közé vesszöt
 szoveg = "kalapács"
 print(szoveg)
 for karakter in szoveg:
-    print(karakter,sep="\nn")
+    print(karakter,end=",")
+print()
+
+
+
+for index in range(len(szoveg)-1,-1,-1):
+    print(szoveg[index],end="")
+print()
+
+
+
+
+
+for index in range(0 , len(szoveg)-1,1):
+    print(szoveg[index]+",",end="")
+print(szoveg[-1])
 
 # pdf 14.-16. feladat
+
+for negy in range(32,51,4):
+    print(negy)
+print()
+
+for csokk in range(10,0,-1):
+    print(csokk,end=" ")
+
+
+
+
+
+#irassa ki a kalapacs szot be kalapacs ki 1k 2a 3l 4a 5p 6a 7c 8s
+
+
+n = len(szoveg)
+
+szoveg = "kalapács"
+index = "kalapács"
+
+for index in range(0,n,1):
+    print(str(index+1)+szoveg[index],end=" ")
