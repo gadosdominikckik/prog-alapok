cdabb7796859c73fe039e7ef46d7470f430eb95f
Dominik <gados.dominik@ckik.hu>
2026-02-04 19:15:45 +0100

    otthoni

--- 2026.01.22 Gyakorlás.py
+++ 2026.01.22 Gyakorlás.py
@@ -1,8 +1,65 @@
 # [-950,950 kozott] x50 x00 tipusu sza,pl
 # [-19,-19] *50
-import random
+import random    
+"""
+ == : Egyenlő
+ != : Nem egyenlő
+"""
+#Írjon fv-t ami visszaadja a listank terjedelmet
 
-def pozitivatlag()
+# def listaTerjed(lista):
+#     maxe = lista[0]
+#     mine = lista[0]
+#     for i in range(1,len(lista),1):
+#         if(lista[i]>maxe):
+#             maxe = lista[i]
+#         if(lista[i]<mine):
+#             mine = lista[i]
+#     terjedelem = maxe - mine
+#     return terjedelem
+
+def maximumErtek(lista):
+    maxe = lista[0]
+    for i in range(1,len(lista),1):
+        if(lista[i]>maxe):
+            maxe = lista[i]
+    return maxe
+
+def minimumErtek(lista):
+    mine = lista[0]
+    for i in range(1,len(lista),1):
+        if(lista[i]<mine):
+            mine = lista[i]
+    return mine
+
+
+
+def terjedelem(lista):
+    maxe = maximumErtek(lista)
+    mine = minimumErtek(lista)
+    return maxe - mine
+
+
+
+def maximumindex(lista):
+    maxi = 0
+    for i in range(1,len(lista),1):
+        if(lista[i]>lista[maxi]):
+            maxi = i
+    return maxi
+
+
+def pozitivatlag(lista):
+    db = 0
+    osszeg = 0
+    for elem in lista:   # végigmegyünk a lista összes elemén
+        if(elem>0):
+            db+=1
+            osszeg += elem
+    atlag = osszeg / db 
+    return atlag
+
+
 
 
 def listaatlaga(lista):
@@ -25,13 +82,24 @@ def veletlenLista(n):
     return(lista)
 
 def main():
-    lista1 = veletlenLista(13)
+    lista1 = veletlenLista(8)
     print(lista1)
-    lista2 = veletlenLista(67)
+    lista2 = veletlenLista(7)
     print(lista2)
 
     lista1atlaga= listaatlaga(lista1)
     print("Az első lista átlaga: ", lista1atlaga)
     print("A második lista átlaga: ",listaatlaga(lista2))
+
+    print("Az első lista pozitív számainak átlaga: " ,
+    pozitivatlag(lista2))
+
+    maxlista1 = maximumindex(lista1)
+    print("Első lista legnagyobb elem helye : ", maxlista1+1)
+
+    print(maximumErtek(lista1))
+    print(minimumErtek(lista1))
+    print(" Az első lista terjedelme:",terjedelem(lista1))
+
 
 main()
